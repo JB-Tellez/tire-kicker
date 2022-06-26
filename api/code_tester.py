bf29fde85b811085fcea6d8b690dda1fe0715b77
JB-Tellez <jb.tellez@gmail.com>
2022-06-26 10:43:17 -0700

    adds logging and GET

--- api/code_tester.py
+++ api/code_tester.py
@@ -2,26 +2,36 @@ from importlib.util import module_from_spec, spec_from_loader
 import sys
 import pytest
 from http.server import BaseHTTPRequestHandler
+import logging
 
 
 class handler(BaseHTTPRequestHandler):
-    def do_POST(self):
+    def _set_response(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-type", "text/html")
         self.end_headers()
-        #         code = """
-        # def add(a, b):
-        #     return a + b
-        #         """
 
+    def do_GET(self):
+        logging.info(
+            "GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers)
+        )
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode("utf-8"))
+
+    def do_POST(self):
         content_length = int(
             self.headers["Content-Length"]
         )  # <--- Gets the size of data
         post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        logging.info(
+            "POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+            str(self.path),
+            str(self.headers),
+            post_data.decode("utf-8"),
+        )
 
-        post_data += "\n# Did you get that?"
-        self.wfile.write(post_data.encode())
-        return
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode("utf-8"))
 
 
 def test_code(code):
